6da5a5fb478baad6a0157a25e1aa97d48587a8f6
Vanchegs <manshin9300@gmail.com>
2025-12-06 18:55:07 +0300

    Initial commit

---
+++ Assets/Internal/Codebase/Character/CharacterMover.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Codebase
+{
+    public class CharacterMover : MonoBehaviour
+    {
+        
+    }
+}
+
---
+++ Assets/Internal/Codebase/StatsSystem/Stat.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Codebase
+{
+    [Serializable]
+    public class Stat
+    {
+        private int maxValue;
+        private int minValue;
+
+        private float currentValue;
+
+        public Stat(int maxValue, int minValue)
+        {
+            this.maxValue = maxValue;
+            this.minValue = minValue;
+        }
+    }
+}
+
