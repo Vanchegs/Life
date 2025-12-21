c59b139f2eb93dc1a28f693195e832a64a615875
Vanchegs <manshin9300@gmail.com>
2025-12-21 23:18:55 +0300

    input update

--- Assets/Internal/Codebase/Character/StateController/StateController.cs
+++ Assets/Internal/Codebase/Character/StateController/StateController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Codebase
@@ -6,6 +7,31 @@ namespace Codebase
     {
         private StatsController statsController;
 
+        public void Update()
+        {
+            CheckInput();
+        }
+
+        private void CheckInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                Debug.Log("Нажата клавиша 1 - Спать");
+            }
+        
+            if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                Debug.Log("Нажата клавиша 2 - Работать");
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+            {
+            }
+
+            switch (typeof(KeyCode))
+            {
+                
+            }
+        }
     }
 }
 
