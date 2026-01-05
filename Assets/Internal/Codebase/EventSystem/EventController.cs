7ae11e87e3a62b3813e8d6b2db0667a4119ac73c
Vanchegs <manshin9300@gmail.com>
2026-01-05 03:23:23 +0300

    minFix

---
+++ Assets/Internal/Codebase/EventSystem/EventController.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Codebase
+{
+    public class EventController : MonoBehaviour
+    {
+        [SerializeField] private List<Event> events;
+    }
+}
+
