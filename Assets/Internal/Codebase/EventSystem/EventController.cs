3cfa1e0be229044ac72106be72d3249a57270366
Vanchegs <manshin9300@gmail.com>
2026-01-07 04:13:04 +0300

    minFix

--- Assets/Internal/Codebase/EventSystem/EventController.cs
+++ Assets/Internal/Codebase/EventSystem/EventController.cs
@@ -6,6 +6,13 @@ namespace Codebase
     public class EventController : MonoBehaviour
     {
         [SerializeField] private List<Event> events;
+
+        private Event GetEvent()
+        {
+            var index = Random.Range(0, events.Count);
+
+            return events[index];
+        }
     }
 }
 
