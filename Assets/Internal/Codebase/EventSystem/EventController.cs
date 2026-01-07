20f3cc2ac387d92919e8540e50b312c83e389326
Vanchegs <manshin9300@gmail.com>
2026-01-07 04:32:29 +0300

    minFix

--- Assets/Internal/Codebase/EventSystem/EventController.cs
+++ Assets/Internal/Codebase/EventSystem/EventController.cs
@@ -1,11 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Codebase
 {
     public class EventController : MonoBehaviour
     {
         [SerializeField] private List<Event> events;
+
+        private void Start()
+        {
+            Debug.Log(GetEvent().eventName);
+        }
 
         private Event GetEvent()
         {
