07be2029e823136bf823f7f6977427b78fbab6ba
Vanchegs <manshin9300@gmail.com>
2026-01-27 19:42:03 +0300

    minFix

--- Assets/Internal/Codebase/EventSystem/EventView.cs
+++ Assets/Internal/Codebase/EventSystem/EventView.cs
@@ -5,14 +5,17 @@ namespace Codebase
 {
     public class EventView : MonoBehaviour
     {
+        [SerializeField] private GameObject eventPanel;
+        
         [SerializeField] private TMP_Text eventNameText;
         [SerializeField] private TMP_Text eventDescriptionText;
-
         [SerializeField] private TMP_Text firstSolutionText;
         [SerializeField] private TMP_Text secondSolutionText;
 
         public void ShowEvent(Event _event)
         {
+            eventPanel.SetActive(true);
+            
             eventNameText.text = _event.eventName;
             eventDescriptionText.text = _event.description;
             firstSolutionText.text = _event.firstSolution;
