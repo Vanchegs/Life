4b06a744e9ff5276ceeb333e2f08185032ad3c8c
Vanchegs <manshin9300@gmail.com>
2026-02-20 05:09:36 +0300

    min fix

--- Assets/Internal/Codebase/WalletSystem/Wallet.cs
+++ Assets/Internal/Codebase/WalletSystem/Wallet.cs
@@ -1,11 +1,16 @@
+using UnityEngine;
+
 namespace Codebase
 {
     public class Wallet
     {
         private int balance;
 
-        public void ChangeBalance(int changeValue) => 
+        public void ChangeBalance(int changeValue)
+        {
             balance += changeValue;
+            Debug.Log(balance);
+        }
 
         public void SetSaveBalance(int savedBalance) => 
             balance = savedBalance;
