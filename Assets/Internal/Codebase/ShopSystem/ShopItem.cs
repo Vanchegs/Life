cf3c6162478e641c91bae18e3baa7e7bb6d18cf9
Vanchegs <manshin9300@gmail.com>
2026-02-20 03:07:07 +0300

    shop item update

---
+++ Assets/Internal/Codebase/ShopSystem/ShopItem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Codebase
+{
+    [CreateAssetMenu(menuName = "Configs/Shop Item Config", fileName = "ShopItem")]
+    public class ShopItem : ScriptableObject
+    {
+        
+    }
+}
+
