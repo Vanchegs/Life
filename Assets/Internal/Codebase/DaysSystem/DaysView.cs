f3dfa31294c55b4761f4f28eabcc3035f63aa13f
Vanchegs <manshin9300@gmail.com>
2026-04-01 21:18:51 +0300

    days system update

---
+++ Assets/Internal/Codebase/DaysSystem/DaysView.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+
+namespace Codebase
+{
+    public class DaysView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text daysText;
+
+        private int dayNumber;
+        
+        
+    }
+}
+
