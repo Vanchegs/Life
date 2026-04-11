a2cf3b7c18cf07a4b8cf4421f2b69014c00c1d1d
Vanchegs <manshin9300@gmail.com>
2026-04-11 19:33:11 +0300

    min fix

--- Assets/Internal/Codebase/ThoughtSystem/ThoughtController.cs
+++ Assets/Internal/Codebase/ThoughtSystem/ThoughtController.cs
@@ -4,7 +4,48 @@ namespace Codebase
 {
     public class ThoughtController : MonoBehaviour
     {
+        [SerializeField] private ThoughtsConfig thoughts;
         
+        private enum ThoughtType
+        {
+            HungryThought,
+            EnergyThought,
+            MentalThought,
+            HealthThought
+        }
+
+        private string GetThought(ThoughtType type)
+        {
+            string thought = null;
+            int randIndex;
+            
+            switch (type)
+            {
+                case ThoughtType.HungryThought:
+                    randIndex = Random.Range(0, thoughts.hungryThoughts.Count);
+                    thought = thoughts.hungryThoughts[randIndex];
+                    break;
+                case ThoughtType.EnergyThought:
+                    randIndex = Random.Range(0, thoughts.energyThoughts.Count);
+                    thought = thoughts.energyThoughts[randIndex];
+                    break;
+                case ThoughtType.HealthThought:
+                    randIndex = Random.Range(0, thoughts.healthThoughts.Count);
+                    thought = thoughts.healthThoughts[randIndex];
+                    break;
+                case ThoughtType.MentalThought:
+                    randIndex = Random.Range(0, thoughts.mentalThoughts.Count);
+                    thought = thoughts.mentalThoughts[randIndex];
+                    break;
+            }
+
+            return thought;
+        }
+
+        private void ShowThought()
+        {
+            
+        }
     }
 }
 
--- Assets/Internal/Codebase/ThoughtSystem/ThoughtView.cs
+++ Assets/Internal/Codebase/ThoughtSystem/ThoughtView.cs
@@ -1,9 +1,49 @@
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 namespace Codebase
 {
     public class ThoughtView : MonoBehaviour
     {
+        [SerializeField] private RectTransform thoughtPanel;
+        [SerializeField] private TMP_Text thoughtText;
         
+        private Vector2 startPosition;
+        private Vector2 endPosition;
+        
+        private void Awake()
+        {
+            startPosition = new Vector2(-300, -500);
+            endPosition = new Vector2(-800, -200);
+            
+            thoughtPanel.anchoredPosition = startPosition;
+            thoughtPanel.gameObject.SetActive(false);
+        }
+        
+        public void ShowThought(string text, float duration = 3f)
+        {
+            StartCoroutine(AnimateThought(text, duration));
+        }
+        
+        private System.Collections.IEnumerator AnimateThought(string text, float duration)
+        {
+            thoughtText.text = text;
+            thoughtPanel.gameObject.SetActive(true);
+            thoughtPanel.anchoredPosition = startPosition;
+            thoughtPanel.localScale = Vector3.one;
+            
+            // Поднятие наверх
+            thoughtPanel.DOAnchorPosY(-150, 0.4f).SetEase(Ease.OutBack);
+            
+            yield return new WaitForSeconds(duration);
+            
+            // Уход влево и исчезновение
+            Sequence hide = DOTween.Sequence();
+            hide.Join(thoughtPanel.DOAnchorPosX(endPosition.x, 0.5f).SetEase(Ease.InBack));
+            hide.Join(thoughtPanel.DOScale(0.8f, 0.5f));
+            hide.OnComplete(() => thoughtPanel.gameObject.SetActive(false));
+            hide.Play();
+        }
     }
 }
