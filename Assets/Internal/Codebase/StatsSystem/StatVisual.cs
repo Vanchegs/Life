989a6c6cb65fbc9bff7647616ff436363b9f644e
Vanchegs <manshin9300@gmail.com>
2026-08-30 23:10:59 +0300

    update status system

--- Assets/Internal/Codebase/StatsSystem/StatVisual.cs
+++ Assets/Internal/Codebase/StatsSystem/StatVisual.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,17 @@ namespace Codebase
         [SerializeField] private Slider foodSlider;
         [SerializeField] private Slider sleepSlider;
         [SerializeField] private Slider healthSlider;
-        [SerializeField] private Slider mentalHealthSlider;
+        [SerializeField] private Slider mentalSlider;
+
+        [SerializeField] private SpriteRenderer energyStatusSprite;
+        [SerializeField] private SpriteRenderer foodStatusSprite;
+        [SerializeField] private SpriteRenderer mentalStatusSprite;
+        [SerializeField] private SpriteRenderer healthStatusSprite;
+
+        [SerializeField] private List<Sprite> energyStatusSprites;
+        [SerializeField] private List<Sprite> foodStatusSprites;
+        [SerializeField] private List<Sprite> mentalStatusSprites;
+        [SerializeField] private List<Sprite> healthStatusSprites;
 
         private Stat foodStat;
         private Stat energyStat;
@@ -20,7 +31,7 @@ namespace Codebase
             InitSlider(foodSlider, statConfig);
             InitSlider(sleepSlider, statConfig);
             InitSlider(healthSlider, statConfig);
-            InitSlider(mentalHealthSlider, statConfig);
+            InitSlider(mentalSlider, statConfig);
         
             this.foodStat = foodStat;
             this.energyStat = energyStat;
@@ -40,7 +51,7 @@ namespace Codebase
         
             slider.minValue = config.minValue;
             slider.maxValue = config.maxValue;
-            slider.value = config.maxValue; // Начальное значение = максимум
+            slider.value = config.maxValue;
         }
     
         public void UpdateValue()
@@ -48,7 +59,35 @@ namespace Codebase
             foodSlider.value = Mathf.Clamp(foodStat.GetCurrentStat(), foodSlider.minValue, foodSlider.maxValue);
             sleepSlider.value = Mathf.Clamp(energyStat.GetCurrentStat(), sleepSlider.minValue, sleepSlider.maxValue);
             healthSlider.value = Mathf.Clamp(healthStat.GetCurrentStat(), healthSlider.minValue, healthSlider.maxValue);
-            mentalHealthSlider.value = Mathf.Clamp(mentalHealthStat.GetCurrentStat(), mentalHealthSlider.minValue, mentalHealthSlider.maxValue);
+            mentalSlider.value = Mathf.Clamp(mentalHealthStat.GetCurrentStat(), mentalSlider.minValue, mentalSlider.maxValue);
+
+            if (foodStat.GetCurrentStat() >= 66)
+                foodStatusSprite.sprite = foodStatusSprites[0];
+            else if (foodStat.GetCurrentStat() >= 33 && foodStat.GetCurrentStat() < 66)
+                foodStatusSprite.sprite = foodStatusSprites[1];
+            else
+                foodStatusSprite.sprite = foodStatusSprites[2];
+            
+            if (energyStat.GetCurrentStat() >= 66)
+                energyStatusSprite.sprite = energyStatusSprites[0];
+            else if (energyStat.GetCurrentStat() >= 33 && energyStat.GetCurrentStat() < 66)
+                energyStatusSprite.sprite = energyStatusSprites[1];
+            else
+                energyStatusSprite.sprite = energyStatusSprites[2];
+
+            if (mentalHealthStat.GetCurrentStat() >= 66)
+                mentalStatusSprite.sprite = mentalStatusSprites[0];
+            else if (mentalHealthStat.GetCurrentStat() >= 33 && mentalHealthStat.GetCurrentStat() < 66)
+                mentalStatusSprite.sprite = mentalStatusSprites[1];
+            else
+                mentalStatusSprite.sprite = mentalStatusSprites[2];
+
+            if (healthStat.GetCurrentStat() >= 66)
+                healthStatusSprite.sprite = healthStatusSprites[0];
+            else if (healthStat.GetCurrentStat() >= 33 && healthStat.GetCurrentStat() < 66)
+                healthStatusSprite.sprite = healthStatusSprites[1];
+            else
+                healthStatusSprite.sprite = healthStatusSprites[2];
         }
     }
 }
